83867ec3aaeebdba208e8ed54f06c424341cb135
MinaSaad47 <MinaSaad-47@outlook.com>
2026-03-16 10:56:00 +0200

    ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: process.env.VITE_SITE_URL,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env instead of just `VITE_` ones.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    // Ensure base always starts and ends with a slash, or default to '/'
+    base: env.VITE_SITE_URL || '/',
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  }
 })
