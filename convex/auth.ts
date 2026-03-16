024e4c031c3849f14330005038274ea4bb55fca8
MinaSaad47 <MinaSaad-47@outlook.com>
2026-03-16 10:45:27 +0200

    ci

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,10 +2,11 @@ import { createClient, type GenericCtx } from "@convex-dev/better-auth";
 import { convex, crossDomain } from "@convex-dev/better-auth/plugins";
 import { betterAuth } from "better-auth/minimal";
 import { components } from "./_generated/api";
-import { DataModel } from "./_generated/dataModel";
+import { type DataModel } from "./_generated/dataModel";
 import { query } from "./_generated/server";
 import authConfig from "./auth.config";
 
+// @ts-ignore
 const siteUrl = process.env.SITE_URL!;
 
 // The component client has methods needed for integrating Convex with Better Auth,
